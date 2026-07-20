b21247d9386b74f3e099781c3ad2d3da73a58cd3
supervoidcoder <supervoidcoder@users.noreply.github.com>
2026-07-20 08:57:24 -0400

    yes

--- container.js
+++ container.js
@@ -1,4 +1,5 @@
 import { Sandbox } from "tensorlake";
+import { loadNotebook, loadTodoList, loadReviews, loadStickyNotes, REVERT_FILE } from "./fs.js";
 let sandbox = null;
 
 async function initializeSandbox() {
