ea6c5d344a92d8d75e556185642f13f43b96e358
AmpElectrecuted <266199358+ampelc@users.noreply.github.com>
2026-09-07 19:10:37 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -376,7 +376,7 @@ async function boxyCommentorIssue(context, app, startCodeReview) {
     return;
   }
 
-  if (!textBody.includes(mentionHandle) && isComment) return;
+  if (!textBody.includes(mentionHandle) && isComment && !author=="coderabbitai") return;
 
   if (textBody.trim() === `${mentionHandle} review` && isPullRequest) {
     // Asynchronicity is beautiful, isn't it?
