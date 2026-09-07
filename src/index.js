d6442ae56f61926ec916d6ced9de6d422c628ce6
AmpElectrecuted <266199358+ampelc@users.noreply.github.com>
2026-09-07 21:27:03 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -485,7 +485,7 @@ async function boxyCommentorIssue(context, app, startCodeReview) {
         if (Object.keys(reviews).length > 0) { 
         }
       }
-      const systemPrompt = `Generate code.
+      const systemPrompt = `Generate code for every mesasge. Jokes are not allowed.
         ${conversationHistory}
       `;
 
