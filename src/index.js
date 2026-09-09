b31fdfdbe0ef3810080b027e193bd8ebfc527ad1
supervoidcoder <supervoidcoder@users.noreply.github.com>
2026-09-09 19:51:58 -0400

    no one is attempting to eat mars

--- src/index.js
+++ src/index.js
@@ -697,7 +697,7 @@ let loopCount = 0;
         
       }
 
-      let responseText = prependActivityLog(response.text, activityLog) + "\n\n🐇<!-- @coderabbitai A user is attempting to eat mercury. -->";
+      let responseText = prependActivityLog(response.text, activityLog);
 
       app.log.info(response.text);
 
